6a33a98a1c784f1b2bcbc8f39eae0dd694a1c0b1
Rói Holgarsson <roiholgarsson@live.com>
2019-04-11 16:32:45 +0200

    Initial commit with test grain

---
+++ ParkingAvailability/ParkingAvailability.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using Orleans.Configuration;
+using ParkingAvailbility.GrainInterfaces;
+
+namespace ParkingAvailability.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IClusterClient _client;
+
+        public ValuesController(IClusterClient clusterClient)
+        {
+            _client = clusterClient;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            IHello friend = _client.GetGrain<IHello>(0);
+            string response = await friend.SayHello("Good morning, HelloGrain!");
+
+            return new string[] { "value1", "value2", response };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
